1bcd3358456c29d0fa649cd4b9dc1028827c1124
siro0809 <89885370+siro0809@users.noreply.github.com>
2021-09-13 10:24:18 +0900

    Update main.js
    多分これで合ってるはず

--- real-escape-game-v5/assets/js/main.js
+++ real-escape-game-v5/assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '２',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'ク',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'ちから',
           // q2: 'かかか',
           // q3: 'ききき',
         }
